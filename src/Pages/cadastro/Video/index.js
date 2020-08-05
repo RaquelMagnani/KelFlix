f04602897ed36c68be0c36340e862dff94c6f577
Raquel.Magnani <quel.magnani88@gmail.com>
2020-08-05 19:33:10 -0300

    add page cadastro and error

---
+++ src/Pages/cadastro/Video/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function CadastroVideo(){
+    return(
+      <div>
+        Pagina de Cadastro de Video
+      </div>
+    )
+  }
+
+  export default CadastroVideo;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './Pages/cadastro/Video'
+
+ const Pagina404 =()=>(<div>Pagina 404</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/cadastro/video" component={CadastroVideo}/>
+      <Route path="/" component={App} exact />
+      <Route  component={Pagina404}/>
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
-
